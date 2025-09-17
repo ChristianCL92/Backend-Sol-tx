2a8f63230318a993a591a6d69d1feacd065573ea
Christian <chrisdev92@outlook.com>
2025-09-17 09:43:53 +0200

    refactor AppModule to include ConfigModule and TypeOrmModule for db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WalletModule } from './modules/wallet/wallet.module';
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => getDatabaseConfig(configService),
+      inject: [ConfigService],
+    }),
+    WalletModule]
 })
 export class AppModule {}
