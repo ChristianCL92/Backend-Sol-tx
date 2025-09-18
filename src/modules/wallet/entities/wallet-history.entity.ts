1840361bcf733cfa34c2c11ad2fdb0f6380d520d
Christian <chrisdev92@outlook.com>
2025-09-18 21:25:41 +0200

    add wallet-history.entity for tracking wallet transaction history

---
+++ src/modules/wallet/entities/wallet-history.entity.ts
@@ -0,0 +1,42 @@
+import { Entity,
+        PrimaryGeneratedColumn,
+        Column,
+        CreateDateColumn,
+        JoinColumn,
+        ManyToOne  
+        } from "typeorm"
+        import { Wallet } from "./wallet.entity";
+
+
+@Entity("wallet_history")
+export class WalletHistory {
+   @PrimaryGeneratedColumn()
+   id:number
+
+  @ManyToOne(() => Wallet, (wallet) => wallet.history)
+  @JoinColumn({ name: 'walletId' })
+  wallet: Wallet;
+
+   @Column()
+   walletId: string
+
+   @Column("decimal", {precision: 18, scale: 12, nullable: true})
+   balance: number
+
+    @Column("decimal",{ precision: 4, scale: 4, nullable: true })
+  spamThreshold: number;
+
+  @Column({nullable: true})
+  preferredTxLimit: number
+
+  @Column()
+  changeType: string; 
+
+  @CreateDateColumn()
+  timestamp: Date;
+
+  
+  @Column({ nullable: true })
+  notes: string;  
+
+}
